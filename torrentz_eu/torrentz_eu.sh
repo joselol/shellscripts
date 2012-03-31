a4ba5f6f9a7072463c3c5e07e41c5ac05619212f
Sairon Istyar <saironiq@gmail.com>
2012-03-31 12:39:40 +0100

    https and cookie change, working again

--- torrentz_eu/torrentz_eu.sh
+++ torrentz_eu/torrentz_eu.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-#
-# by Sairon Istyar, 2012
-# distributed under the GPLv3 license
-# http://www.opensource.org/licenses/gpl-3.0.html
-#
+##########################
+# by Sairon Istyar, 2012 #
+##########################
 
 # program to use for torrent download
-program='/usr/bin/transmission-remote'
+program='/opt/usr/bin/transmission-remote'
 progopts='-a'
 # show N first matches by default
 limit=15
@@ -16,84 +14,40 @@ namecolor='\x1b[1;33m'
 sizecolor='\x1b[1;36m'
 seedcolor='\x1b[1;31m'
 peercolor='\x1b[1;32m'
-errocolor='\x1b[1;31m'
-mesgcolor='\x1b[1;37m'
 nonecolor='\x1b[0m'
 
-thisfile="$0"
-
 printhelp() {
-	echo -e "Usage:"
-	echo -e "\t$thisfile [options] search query"
-	echo
-	echo
-	echo -e "Available options:"
-	echo -e "\t-h\t\tShow help"
-	echo -e "\t-n [num]\tShow only first N results (default 15; max 50)"
-	echo -e "\t-C\t\tDo not use colors"
-	echo -e "\t-P [prog]\tSet torrent client"
-	echo -e "\t-O [opts]\tSet torrent client cmdline options"
-	echo
-	echo -e "Current client settings: $program $progopts [magnet link]"
+	echo -e "Usage:\n\t$0 [options] search query\n\nAvailable options:\n\t-h\t\tShow help\n\t-n [num]\tShow only first N results (default 15; max 50)\n\t-C\t\tDo not use colors"
 }
 
-# change torrent client
-chex() {
-	sed "s!^program=.*!program=\'$program\'!" -i "$thisfile"
-	if [ $? -eq 0 ] ; then
-		echo "Client changed successfully."
-		exit 0
-	else
-		echo -e "${errocolor}(EE) ${mesgcolor}==> Something went wrong!${nonecolor}"
-		exit 1
-	fi
-}
-
-# change torrent client options
-charg() {
-	sed "s!^progopts=.*!progopts=\'$progopts\'!" -i "$thisfile"
-	if [ $? -eq 0 ] ; then
-                echo "Cmdline options for client changed successfully."
-                exit 0
-        else 
-                echo -e "${errocolor}(EE) ${mesgcolor}==> Something went wrong!${nonecolor}"
-                exit 1
-        fi
-}
-
-# script cmdline option handling
-while getopts :hn:CP:O:: opt ; do
+while getopts :hCn: opt ; do
 	case "$opt" in
 		h) printhelp; exit 0;;
 		n) limit="$OPTARG";;
-		C) unset numbcolor namecolor sizecolor seedcolor peercolor nonecolor errocolor mesgcolor;;
-		P) program="$OPTARG"; chex;;
-		O) progopts="$OPTARG"; charg;;
+		C) unset numbcolor; unset namecolor; unset sizecolor; unset seedcolor; unset peercolor; unset nonecolor;;
 		*) echo -e "Unknown option(s)."; printhelp; exit 1;;
 	esac
 done
 
 shift `expr $OPTIND - 1`
 
-# correctly encode query
 q=`echo "$*" | tr -d '\n' | od -t x1 -A n | tr ' ' '%'`
 
-# get results
-r=`curl -s "http://torrentz.eu/search?f=$q" \
+cookie=`date +"%a %b %d %Y %T GMT%z (%Z)"`
+
+r=`curl -A Mozilla -b "_SESS=$cookie" -s "https://torrentz.eu/search?f=$q" \
 	| grep -Eo '<dl><dt><a href=\"\/[[:alnum:]]*\">.*</a>|<span class=\"[speud]*\">[^<]*</span>' \
-	| sed 's!<dl><dt><a href=\"/!!; \
+	| sed  's!<dl><dt><a href=\"/!!; \
 		s!\">!|!; \
 		s!<[/]*b>!!g; \
 		N;N;N;s!\n<span class=\"[pesud]*\">!|!g; \
 		s!</span>!!g; \
 		s!</a>!!'`
 
-# number of results
 n=`echo "$r" | wc -l`
 
 IFS=$'\n'
 
-# print results
 echo "$r" \
 	| head -n "$limit" \
 	| awk -v N=1 \
@@ -106,11 +60,10 @@ echo "$r" \
 		-F '|' \
 		'{print NU N ") " NA $2 " " SI $3 " " SE $4 " " PE $5 NO; N++}'
 
-# read ID, ignore every character except digits
 read -p ">> Enter torrent No. to download: " down
+
 down=`echo "$down" | sed 's/[^[:digit:]]*//g' | head -n 1`
 
-# check if ID is valid and in range of results, download torrent
 if [ -z "$down" ] ; then
 	echo "Not a number!"
 	unset IFS
